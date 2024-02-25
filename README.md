1130da12f393fd3f6a283babea057fa152972edc
QuantumHW <1415821217@qq.com>
2024-02-25 22:34:33 +0800

    First submission

---
+++ README.md
@@ -0,0 +1,28 @@
+# 智能花卉识别系统
+
+## 环境配置
+
+**Python>=3.8**
+
+**PyTorch>=1.8**
+
+## 数据集标签索引
+
+| 标签              | 含义     |
+| :---------------- | :------- |
+| daisy             | 雏菊     |
+| dandelion         | 蒲公英   |
+| Rose              | 玫瑰     |
+| sunflower         | 向日葵   |
+| champaka          |          |
+| chitrak           |          |
+| Common Lanthana   | 五色梅   |
+| Hibiscus          | 木槿花   |
+| honeysuckle       | 金银花   |
+| indian mallow     | 梵天花   |
+| Jatropha          | 琴叶珊瑚 |
+| malabar melastome | 野牡丹   |
+| Marigold          | 万寿菊   |
+| shankupushpam     |          |
+| spider lily       | 彼岸花   |
+
---
+++ main.py
@@ -0,0 +1,162 @@
+"""
+@author: Huang Wen
+@file: main.py
+@time: 2024/2/20 17:51
+@desc:
+"""
+
+import os
+import cv2
+import sys
+import time
+import torch
+from ultralytics import YOLO
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from MainUI import Ui_MainWindow
+
+
+_translate = QtCore.QCoreApplication.translate
+weights_path = None  # 存放权重文件的路径
+image_path = ''  # 存放待检测文件的路径
+camera_flag = False  # 存储摄像头的状态，初始为关闭（False）
+model = None  # 存放检测模型
+cap = cv2.VideoCapture(0)
+
+
+def load_weights():
+    file_path, file_type = QFileDialog.getOpenFileNames(MainWindow, '选择权重文件', os.getcwd(),
+                                                        "PT文件(*.pt);;所有文件(*)")
+    if file_path:
+        try:
+            global weights_path, model
+            weights_path = str(file_path[0])
+            model = YOLO(weights_path)
+            out_text('已加载权重：'+weights_path)
+        except Exception as e:
+            out_text('Exception:'+str(e))
+
+
+def open_file():
+    global image_path, cap
+    file_path, _ = QFileDialog.getOpenFileNames(MainWindow, '选择需要检测的图像或视频', os.getcwd(),
+                                                        "所有文件(*);;图像文件(*.jpg;*.jpeg;*.png);;视频文件(*.mp4;*.avi)")
+    if file_path:
+        if not weights_path:
+            out_text('文件加载失败，请先加载权重文件！')
+            return
+        ui.button_camera.setText(_translate("MainWindow", "打开摄像头"))
+        file_type = file_path[0].split('.')[1]
+        image_path = str(file_path[0])
+        if file_type in ['mp4', 'avi']:
+            try:
+                cap = cv2.VideoCapture(image_path)
+                if cap.isOpened():
+                    out_text('已加载视频：' + image_path)
+                    timer_pic.start(5)
+                else:
+                    out_text('视频打开失败！')
+            except Exception as e:
+                out_text('Exception:'+str(e))
+        else:
+            try:
+                cap.release()  # 如果有正在播放的视频，释放掉
+                # img = cv2.imread(image_path)
+                out_text('已加载图像：' + image_path)
+                if weights_path:  # 如果加载了权重文件，则执行检测功能
+                    results = model(image_path)   # 调用检测模型
+                    img = results[0].plot()
+                    cur_frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                    # 视频流的长和宽
+                    height, width = cur_frame.shape[:2]
+                    pixmap = QImage(cur_frame, width, height, QImage.Format_RGB888)
+                    pixmap = QPixmap.fromImage(pixmap)
+                    # 获取是视频流和label窗口的长宽比值的最大值，适应label窗口播放，不然显示不全
+                    ratio = max(width / ui.label_show.width(), height / ui.label_show.height())
+                    pixmap.setDevicePixelRatio(ratio)
+                    # 视频流置于label中间部分播放
+                    ui.label_show.setAlignment(Qt.AlignCenter)
+                    ui.label_show.setPixmap(pixmap)
+            except Exception as e:
+                out_text('Exception:'+str(e))
+
+def open_camera():
+    global camera_flag, weights_path, cap
+    if not camera_flag:
+        if not weights_path:
+            out_text('摄像头打开失败，请先加载权重文件！')
+            return
+        # 打开摄像头
+        camera_flag = True
+        cap = cv2.VideoCapture(0)
+        ui.button_camera.setText(_translate("MainWindow", "关闭摄像头"))
+        out_text('摄像头已打开')
+        timer_pic.start(5)
+    else:
+        # 关闭摄像头
+        camera_flag = False
+        timer_pic.stop()
+        cap.release()
+        ui.button_camera.setText(_translate("MainWindow", "打开摄像头"))
+        ui.label_show.setText(_translate("MainWindow", "暂无数据输入"))
+        out_text('摄像头已关闭')
+
+
+def clean():
+    global cap, camera_flag
+    camera_flag = False
+    cap.release()
+    timer_pic.stop()
+    ui.label_show.setText(_translate("MainWindow", "暂无数据输入"))
+    ui.text_output.setHtml(_translate("MainWindow", " "))
+    ui.button_camera.setText(_translate("MainWindow", "打开摄像头"))
+
+
+def show_pic():
+    # 执行检测功能
+    global cap
+    try:
+        ret, img = cap.read()
+        if ret:
+            # 对每一帧进行检测
+            with torch.no_grad():
+                results = model(img)  # 调用检测模型
+                img = results[0].plot()   # 调用检测模型
+
+            cur_frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            # 视频流的长和宽
+            height, width = cur_frame.shape[:2]
+            pixmap = QImage(cur_frame, width, height, QImage.Format_RGB888)
+            pixmap = QPixmap.fromImage(pixmap)
+            # 获取是视频流和label窗口的长宽比值的最大值，适应label窗口播放，不然显示不全
+            ratio = max(width / ui.label_show.width(), height / ui.label_show.height())
+            pixmap.setDevicePixelRatio(ratio)
+            # 视频流置于label中间部分播放
+            ui.label_show.setAlignment(Qt.AlignCenter)
+            ui.label_show.setPixmap(pixmap)
+    except Exception as e:
+        out_text('Exception：'+str(e))
+
+
+def out_text(text):  # 输出内容至“打印输出”区域
+    timestamp = '[' + time.strftime("%Y/%m/%d-%H:%M") + ']'
+    ui.text_output.append(timestamp + text)
+
+
+if __name__ == '__main__':
+    # train_flowers()  # 训练
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()  # 显示主界面
+    ui.button_weights.clicked.connect(load_weights)  # 将按钮button_weights绑定至函数load_weights
+    ui.button_file.clicked.connect(open_file)  # 将按钮button_file绑定至函数open_file
+    ui.button_camera.clicked.connect(open_camera)  # 将按钮button_camera绑定至函数open_camera
+    ui.button_clean.clicked.connect(clean)  # 将按钮button_clean绑定至函数clean
+    timer_pic = QTimer()
+    timer_pic.timeout.connect(show_pic)
+    sys.exit(app.exec_())
