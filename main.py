f8fbbbf9459ef0b21131dbee8ae1ddbd87283eb0
QuantumHW <1415821217@qq.com>
2024-02-26 17:48:20 +0800

    新增暂停按钮，优化输出逻辑

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ import sys
 import time
 import torch
 from ultralytics import YOLO
-from PyQt5 import QtCore
 from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtGui, QtWidgets
 from MainUI import Ui_MainWindow
 
 
@@ -23,25 +23,35 @@ _translate = QtCore.QCoreApplication.translate
 weights_path = None  # 存放权重文件的路径
 image_path = ''  # 存放待检测文件的路径
 camera_flag = False  # 存储摄像头的状态，初始为关闭（False）
+playing = False
 model = None  # 存放检测模型
+icon_start = QtGui.QIcon()
+icon_pause = QtGui.QIcon()
+
+
 cap = cv2.VideoCapture(0)
+
+my_class = {0: '雏菊', 1: '蒲公英', 2: '玫瑰花', 3: '向日葵', 4: 'champaka', 5: 'chitrak', 6: '五色梅',
+            7: '木槿花', 8: '金银花', 9: '梵天花', 10: '琴叶珊瑚', 11: '野牡丹',
+            12: '万寿菊', 13: 'shankupushpam', 14: '彼岸花'}
 
 
 def load_weights():
     file_path, file_type = QFileDialog.getOpenFileNames(MainWindow, '选择权重文件', os.getcwd(),
                                                         "PT文件(*.pt);;所有文件(*)")
+    global weights_path, model
     if file_path:
         try:
-            global weights_path, model
             weights_path = str(file_path[0])
             model = YOLO(weights_path)
-            out_text('已加载权重：'+weights_path)
         except Exception as e:
             out_text('Exception:'+str(e))
+            return
+        out_text('已加载权重：' + weights_path)
 
 
 def open_file():
-    global image_path, cap
+    global image_path, cap, playing, icon_pause
     file_path, _ = QFileDialog.getOpenFileNames(MainWindow, '选择需要检测的图像或视频', os.getcwd(),
                                                         "所有文件(*);;图像文件(*.jpg;*.jpeg;*.png);;视频文件(*.mp4;*.avi)")
     if file_path:
@@ -57,6 +67,10 @@ def open_file():
                 if cap.isOpened():
                     out_text('已加载视频：' + image_path)
                     timer_pic.start(5)
+                    playing = True
+                    ui.button_playing.setVisible(True)
+                    ui.button_playing.setText(_translate("MainWindow", "暂停"))
+                    ui.button_playing.setIcon(icon_pause)
                 else:
                     out_text('视频打开失败！')
             except Exception as e:
@@ -64,10 +78,21 @@ def open_file():
         else:
             try:
                 cap.release()  # 如果有正在播放的视频，释放掉
+                ui.button_playing.setVisible(False)  # 隐藏按钮
                 # img = cv2.imread(image_path)
                 out_text('已加载图像：' + image_path)
                 if weights_path:  # 如果加载了权重文件，则执行检测功能
                     results = model(image_path)   # 调用检测模型
+
+                    boxes_temp = results[0].boxes
+                    boxes = boxes_temp.xyxy.tolist()
+                    names = boxes_temp.cls.tolist()
+                    conf = boxes_temp.conf.tolist()
+                    for i, box in enumerate(boxes):
+                        out_text('检测到：'+my_class[names[i]]
+                                 + '；位置坐标为：'+str(list(map(int, box)))
+                                 + '；置信度为：'+str(round(conf[i], 2)))
+
                     img = results[0].plot()
                     cur_frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
                     # 视频流的长和宽
@@ -83,14 +108,19 @@ def open_file():
             except Exception as e:
                 out_text('Exception:'+str(e))
 
+
 def open_camera():
-    global camera_flag, weights_path, cap
+    global camera_flag, weights_path, cap, playing, icon_pause
     if not camera_flag:
         if not weights_path:
             out_text('摄像头打开失败，请先加载权重文件！')
             return
         # 打开摄像头
         camera_flag = True
+        playing = True
+        ui.button_playing.setVisible(True)
+        ui.button_playing.setText(_translate("MainWindow", "暂停"))
+        ui.button_playing.setIcon(icon_pause)
         cap = cv2.VideoCapture(0)
         ui.button_camera.setText(_translate("MainWindow", "关闭摄像头"))
         out_text('摄像头已打开')
@@ -98,10 +128,12 @@ def open_camera():
     else:
         # 关闭摄像头
         camera_flag = False
+        playing = False
         timer_pic.stop()
         cap.release()
         ui.button_camera.setText(_translate("MainWindow", "打开摄像头"))
         ui.label_show.setText(_translate("MainWindow", "暂无数据输入"))
+        ui.button_playing.setVisible(False)  # 隐藏按钮
         out_text('摄像头已关闭')
 
 
@@ -113,6 +145,21 @@ def clean():
     ui.label_show.setText(_translate("MainWindow", "暂无数据输入"))
     ui.text_output.setHtml(_translate("MainWindow", " "))
     ui.button_camera.setText(_translate("MainWindow", "打开摄像头"))
+    ui.button_playing.setVisible(False)  # 隐藏按钮
+
+
+def pause_and_start():
+    global playing, icon_start, icon_pause
+    if playing:
+        playing = False
+        ui.button_playing.setText(_translate("MainWindow", "播放"))
+        ui.button_playing.setIcon(icon_start)
+        timer_pic.stop()
+    else:
+        playing = True
+        ui.button_playing.setText(_translate("MainWindow", "暂停"))
+        ui.button_playing.setIcon(icon_pause)
+        timer_pic.start(5)
 
 
 def show_pic():
@@ -125,6 +172,14 @@ def show_pic():
             with torch.no_grad():
                 results = model(img)  # 调用检测模型
                 img = results[0].plot()   # 调用检测模型
+                boxes_temp = results[0].boxes
+                boxes = boxes_temp.xyxy.tolist()
+                names = boxes_temp.cls.tolist()
+                conf = boxes_temp.conf.tolist()
+                for i, box in enumerate(boxes):
+                    out_text('检测到：' + my_class[names[i]]
+                             + '；位置坐标为：' + str(list(map(int, box)))
+                             + '；置信度为：' + str(round(conf[i], 2)))
 
             cur_frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             # 视频流的长和宽
@@ -153,10 +208,15 @@ if __name__ == '__main__':
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()  # 显示主界面
+    icon_start.addPixmap(QtGui.QPixmap("source/start.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+    icon_pause.addPixmap(QtGui.QPixmap("source/pause.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+
     ui.button_weights.clicked.connect(load_weights)  # 将按钮button_weights绑定至函数load_weights
     ui.button_file.clicked.connect(open_file)  # 将按钮button_file绑定至函数open_file
     ui.button_camera.clicked.connect(open_camera)  # 将按钮button_camera绑定至函数open_camera
+    ui.button_playing.clicked.connect(pause_and_start)  # 将按钮button_playing绑定至函数pause_and_start
     ui.button_clean.clicked.connect(clean)  # 将按钮button_clean绑定至函数clean
+    ui.button_playing.setVisible(False)  # 隐藏按钮
     timer_pic = QTimer()
     timer_pic.timeout.connect(show_pic)
     sys.exit(app.exec_())
